7e00c57dad814bc4de10805474909930325718f5
DucSine <duchoangvo97@gmail.com>
2021-01-15 17:47:47 +0700

    tái cấu trúc design
    Signed-off-by: DucSine <duchoangvo97@gmail.com>

--- app/src/main/java/cdio4/cots/foodoffer/AboutUsActivity.java
+++ app/src/main/java/cdio4/cots/foodoffer/AboutUsActivity.java
@@ -3,13 +3,9 @@ package cdio4.cots.foodoffer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
-import cdio4.cots.foodoffer.dialog.LoginDialog;
 
 public class AboutUsActivity extends AppCompatActivity {
 // trang giới thiệu về hệ thống
@@ -29,19 +25,5 @@ public class AboutUsActivity extends AppCompatActivity {
         inflater.inflate(R.menu.menu_toolbar_main, menu);
         return true;
     }
-
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                return true;
-            case R.id.toolbar_search:
-                return true;
-            case R.id.toolbar_avatar:
-                new LoginDialog(this).show();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    
 }
