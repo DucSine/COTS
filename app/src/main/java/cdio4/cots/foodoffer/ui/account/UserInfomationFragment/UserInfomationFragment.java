f03b9bab4efb0139083b10e5ca0c90aa184d2a2c
DucSine <duchoangvo97@gmail.com>
2021-01-17 04:18:25 +0700

    commit fix form
    Signed-off-by: DucSine <duchoangvo97@gmail.com>

--- app/src/main/java/cdio4/cots/foodoffer/ui/account/UserInfomationFragment/UserInfomationFragment.java
+++ app/src/main/java/cdio4/cots/foodoffer/ui/account/UserInfomationFragment/UserInfomationFragment.java
@@ -40,6 +40,34 @@ public class UserInfomationFragment extends Fragment {
         userInfomationViewModel = new ViewModelProvider(this).get(UserInfomationViewModel.class);
         // TODO: Use the ViewModel
     }
+
+    /*
+    private void InitLayout(){
+        setContentView(R.layout.activity_user_infomation);
+        imgv_avatar = findViewById(R.id.imgv_us_info_avatar);
+
+        edt_usFullnameLayout = findViewById(R.id.ip_layout_us_info_fullname);
+        edt_usFullname = findViewById(R.id.ip_edt_us_info_fullname);
+
+        edt_usBdateLayout = findViewById(R.id.ip_layout_us_info_bdate);
+        edt_usBdate = findViewById(R.id.ip_edt_us_info_bdate);
+
+        edt_usIDLayout = findViewById(R.id.ip_layout_us_info_id);
+        edt_usID = findViewById(R.id.ip_edt_us_info_id);
+
+        edt_usPhoneLayout = findViewById(R.id.ip_layout_us_info_phone);
+        edt_usPhone = findViewById(R.id.ip_edt_us_info_phone);
+
+        edt_usEmailLayout = findViewById(R.id.ip_layout_us_info_email);
+        edt_usEmail = findViewById(R.id.ip_edt_us_info_email);
+
+        edt_usAdressLayout = findViewById(R.id.ip_layout_us_info_adress);
+        edt_usAdress = findViewById(R.id.ip_edt_us_info_adress);
+
+        rbtGroupGender = findViewById(R.id.rbt_group_us_info_gender);
+        rbt_usMale = rbtGroupGender.findViewById(R.id.rbt_us_info_male);
+        rbt_usFemale = rbtGroupGender.findViewById(R.id.rbt_us_info_female);
+     */
     private UserInfomationViewModel userInfomationViewModel;
     private View root;
 
@@ -67,7 +95,8 @@ public class UserInfomationFragment extends Fragment {
     private String phone = "";
     private String email = "";
     private String address = "";
-
+    //Pattern pattern = Pattern.compile("\\d{1,2}[-|/]\\d{1,2}[-|/]\\d{4}");
+    //Matcher matcher = pattern.matcher(text1);
     private TextWatcher edt_fullName_Event = new TextWatcher() {
         @Override
         public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
