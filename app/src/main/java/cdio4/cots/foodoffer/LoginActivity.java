5b432abe2f921c658f9fa1a64cc8ddf1e3fc759b
maxvlassenko1997 <maxvlassenko1997@gmail.com>
2021-01-19 09:43:57 +0700

    Thêm List food cho trang chủ

--- app/src/main/java/cdio4/cots/foodoffer/LoginActivity.java
+++ app/src/main/java/cdio4/cots/foodoffer/LoginActivity.java
@@ -2,13 +2,107 @@ package cdio4.cots.foodoffer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import cdio4.cots.foodoffer.regx.RegularExpression;
 
 public class LoginActivity extends AppCompatActivity {
+    Button btnSignUp;
+    private RegularExpression regularExpression = new RegularExpression();
 
+    private TextInputLayout edt_userNameLayout;
+    private TextInputLayout edt_passwordLayout;
+    private TextInputLayout edt_passwordConfirmLayout;
+    private TextInputEditText edt_userName;
+    private TextInputEditText edt_password;
+    private TextInputEditText edt_passwordConfirm;
+
+    private String userName = "";
+    private String password = "";
+    private String passwordConfirm = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        init();
+        edt_userName.addTextChangedListener(edt_userName_Event);
+        edt_password.addTextChangedListener(edt_password_Event);
+        edt_passwordConfirm.addTextChangedListener(edt_passswordConfirm_Event);
+
     }
+
+    private void init() {
+        edt_userNameLayout = findViewById(R.id.ip_layout_activity_singin_username);
+        edt_passwordLayout = findViewById(R.id.ip_layout_activity_signin_password);
+        edt_passwordConfirmLayout = findViewById(R.id.ip_layout_activity_signin_password_confirm);
+
+        edt_userName = findViewById(R.id.ip_edt_activity_signin_username);
+        edt_password = findViewById(R.id.ip_edt_activity_signin_password);
+        edt_passwordConfirm = findViewById(R.id.ip_edt_activity_signin_password_confirm);
+        btnSignUp = findViewById(R.id.btn_activity_login_sign_up);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+            }
+        });
+    }
+    private TextWatcher edt_userName_Event = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2){
+            regularExpression.checkNull(edt_userNameLayout,charSequence,"Vui lòng nhập tài khoản");
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            userName = edt_userName.getText().toString();
+        }
+    };
+    private TextWatcher edt_password_Event = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            regularExpression.checkNull(edt_userNameLayout,charSequence,"Vui lòng nhập mật khẩu");
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            password = edt_password.getText().toString();
+        }
+    };
+    private TextWatcher edt_passswordConfirm_Event = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            regularExpression.checkNull(edt_userNameLayout,charSequence,"Vui lòng nhập lại mật khẩu");
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            passwordConfirm = edt_passwordConfirm.getText().toString();
+        }
+    };
+
+
 }
--- app/src/main/java/cdio4/cots/foodoffer/ui/home/Fragment/Hot/HotFragment.java
+++ app/src/main/java/cdio4/cots/foodoffer/ui/home/Fragment/Hot/HotFragment.java
@@ -7,32 +7,76 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cdio4.cots.foodoffer.MainActivity;
 import cdio4.cots.foodoffer.R;
+import cdio4.cots.foodoffer.adapter.FoodAdapter;
+import cdio4.cots.foodoffer.model.Food;
 
 public class HotFragment extends Fragment {
 
-    private HotViewModel mViewModel;
+   // private HotViewModel mViewModel;
+    private RecyclerView rcvFood;
+    MainActivity mainActivity;
+    private View mView;
 
-    public static HotFragment newInstance() {
+   /* public static HotFragment newInstance() {
         return new HotFragment();
-    }
+    }*/
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.hot_fragment, container, false);
+
+        mView= inflater.inflate(R.layout.hot_fragment, container, false);
+        mainActivity=(MainActivity) getActivity();
+        rcvFood= mView.findViewById(R.id.rcv_fragment_hot_food);
+        GridLayoutManager gridLayoutManager=new GridLayoutManager(mainActivity,2);
+        rcvFood.setLayoutManager(gridLayoutManager);
+        FoodAdapter adapter = new FoodAdapter(getListFood());
+        rcvFood.setAdapter(adapter);
+        //init();
+        return mView;
     }
 
-    @Override
+    private void init() {
+        rcvFood= mView.findViewById(R.id.rcv_fragment_hot_food);
+        GridLayoutManager gridLayoutManager=new GridLayoutManager(mView.getContext(),2);
+        rcvFood.setLayoutManager(gridLayoutManager);
+
+        FoodAdapter adapter = new FoodAdapter(getListFood());
+        rcvFood.setAdapter(adapter);
+    }
+
+    private List<Food> getListFood() {
+        List<Food> list = new ArrayList<>();
+        for (int i=0;i<=9;i++){
+            list.add(new Food(R.drawable.ic_cafe,"Cafe",10000));
+        }
+        for (int i=0;i<=9;i++){
+            list.add(new Food(R.drawable.ic_trasua,"Trà sữa",20000));
+        }
+        for (int i=0;i<=9;i++){
+            list.add(new Food(R.drawable.ic_suachua,"Sữa chua",30000));
+        }
+        return list;
+    }
+
+/*    @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(HotViewModel.class);
         // TODO: Use the ViewModel
-    }
+    }*/
 
 }
