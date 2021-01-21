ac62bae8becf776c683264d1cc07262832ad6f89
maxvlassenko1997 <maxvlassenko1997@gmail.com>
2021-01-21 17:06:44 +0700

    Ta có chú thích cho m đọc dễ hiểu hơn đó

--- app/src/main/java/cdio4/cots/foodoffer/ui/HomeFragment/Fragment/Hot/HotFragment.java
+++ app/src/main/java/cdio4/cots/foodoffer/ui/HomeFragment/Fragment/Hot/HotFragment.java
@@ -9,12 +9,23 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cdio4.cots.foodoffer.MainActivity;
 import cdio4.cots.foodoffer.R;
+import cdio4.cots.foodoffer.adapter.HotFoodAdapter;
+import cdio4.cots.foodoffer.model.Food;
 
 public class HotFragment extends Fragment {
 
-    private HotViewModel mViewModel;
+    //private HotViewModel mViewModel;
+    private RecyclerView rcvFood;
+    MainActivity mainActivity; //khai báo MainActivity
+    private View mView;
 
     public static HotFragment newInstance() {
         return new HotFragment();
@@ -23,14 +34,38 @@ public class HotFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.hot_fragment, container, false);
+        mView= inflater.inflate(R.layout.hot_fragment, container, false);
+        init();
+        return mView;
     }
 
-    @Override
+    private void init() {
+        mainActivity=(MainActivity) getActivity(); //getActivity trả về Activity chứa Fragment đang gọi, không cần thông qua home, ép kiểu về MainActivity. Cái này có thể dùng để gởi dữ liệu từ Fragment về Activity mà không cần xài interface
+        rcvFood= mView.findViewById(R.id.rcv_fragment_hot_food);
+        GridLayoutManager gridLayoutManager=new GridLayoutManager(mainActivity,2);//định dạng layout cho RecycleView là Grid, 2 cột
+        rcvFood.setLayoutManager(gridLayoutManager);
+        HotFoodAdapter adapter = new HotFoodAdapter(getListFood());
+        rcvFood.setAdapter(adapter);
+    }
+    private List<Food> getListFood() {
+        List<Food> list = new ArrayList<>();
+        for (int i=0;i<=9;i++){
+            list.add(new Food(R.drawable.ic_cafe,"Cafe",10000));
+        }
+        for (int i=0;i<=9;i++){
+            list.add(new Food(R.drawable.ic_trasua,"Trà sữa",20000));
+        }
+        for (int i=0;i<=9;i++){
+            list.add(new Food(R.drawable.ic_suachua,"Sữa chua",30000));
+        }
+        return list;
+    }
+
+   /* @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(HotViewModel.class);
         // TODO: Use the ViewModel
-    }
+    }*/
 
 }
